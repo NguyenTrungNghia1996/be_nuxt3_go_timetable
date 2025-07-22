339166d27015fbf57aab118c9fe6e5278c3cc00e
Nguyễn Trung Nghĩa <59058840+NguyenTrungNghia1996@users.noreply.github.com>
2025-07-21 19:17:27 -0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"go-fiber-api/config"
+	"go-fiber-api/routes"
+	"go-fiber-api/seed"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if _, err := os.Stat(".env"); err == nil {
+		if err := godotenv.Load(); err != nil {
+			log.Println("Error loading .env file")
+		} else {
+			log.Println("Loaded .env file")
+		}
+	}
+
+	// Kết nối MongoDB một lần duy nhất
+	config.ConnectDB()
+
+	// Seed default accounts if needed
+	seed.SeedRoleGroups()
+	seed.SeedAdminUser()
+	seed.SeedDefaultUser()
+	seed.SeedMenus()
+
+	app := fiber.New()
+	app.Use(cors.New())
+	routes.Setup(app, config.DB)
+
+	port := os.Getenv("PORT")
+	log.Fatal(app.Listen(":" + port))
+}
